0c1124b65718ea46a1096cfe5f399b9bd691e25f
Kevin Thomas <kevin@admod.com>
2026-06-05 11:56:32 +0530

    Add priority back to read-only regions

--- atlas.tf
+++ atlas.tf
@@ -1,15 +1,14 @@
+locals {
+  region_list = [for k, v in var.regions : merge(v, { key = k })]
+
+  primary_region    = local.region_list[0]
+  secondary_regions = slice(local.region_list, 1, length(local.region_list))
+}
+
 resource "mongodbatlas_advanced_cluster" "cluster" {
   project_id   = var.atlas_project_id
   name         = var.cluster_name
   cluster_type = "REPLICASET"
-
-  # Ensure deterministic ordering of regions
-  locals {
-    region_list = [for k, v in var.regions : merge(v, { key = k })]
-
-    primary_region    = local.region_list[0]
-    secondary_regions = slice(local.region_list, 1, length(local.region_list))
-  }
 
   # Primary region (electable)
   replication_specs {
@@ -32,7 +31,7 @@ resource "mongodbatlas_advanced_cluster" "cluster" {
         provider_name = "AZURE"
         region_name   = region_configs.value.atlas_region
 
-        # ❌ IMPORTANT: no priority allowed here
+        # ❌ DO NOT set priority here
 
         read_only_specs {
           instance_size = region_configs.value.instance_size
