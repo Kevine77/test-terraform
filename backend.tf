edb73ab4d32c0a4316952375716af609af53265f
Kevin Thomas <kevin@admod.com>
2026-06-05 09:17:00 +0530

    updated yml

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
+    resource_group_name  = "north-europa"
     storage_account_name = "tfstatekevin001"
     container_name       = "tfstate"
     key                  = "mongodb-atlas/prod.tfstate"
