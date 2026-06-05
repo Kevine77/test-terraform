f30b3cd26f5953fd28b17de5314b94447f09f2d4
Kevin Thomas <kevin@admod.com>
2026-06-05 11:47:35 +0530

    Fix multi-region topology - use REPLICASET with read replicas

--- atlas.tf
+++ atlas.tf
@@ -2,20 +2,31 @@
 resource "mongodbatlas_advanced_cluster" "cluster" {
   project_id   = var.atlas_project_id
   name         = var.cluster_name
-  cluster_type = length(var.regions) > 1 ? "SHARDED" : "REPLICASET"
+  cluster_type = "REPLICASET"
 
-  # Dynamically create replication specs for each region
-  dynamic "replication_specs" {
-    for_each = var.regions
-    content {
-      region_configs {
+  # Multi-region replica set - primary and secondary regions
+  replication_specs {
+    # Primary region (first region in map)
+    region_configs {
+      provider_name = "AZURE"
+      region_name   = var.regions[keys(var.regions)[0]].atlas_region
+      priority      = 7
+      electable_specs {
+        instance_size = var.regions[keys(var.regions)[0]].instance_size
+        node_count    = var.regions[keys(var.regions)[0]].node_count
+      }
+    }
+
+    # Secondary regions (remaining regions in map)
+    dynamic "region_configs" {
+      for_each = slice(keys(var.regions), 1, length(var.regions))
+      content {
         provider_name = "AZURE"
-        region_name   = replication_specs.value.atlas_region
-        priority      = replication_specs.value.priority
-
-        electable_specs {
-          instance_size = replication_specs.value.instance_size
-          node_count    = replication_specs.value.node_count
+        region_name   = var.regions[region_configs.value].atlas_region
+        priority      = var.regions[region_configs.value].priority
+        read_only_specs {
+          instance_size = var.regions[region_configs.value].instance_size
+          node_count    = var.regions[region_configs.value].node_count
         }
       }
     }
