74dcae8198eb963e0d75f2c629539ef4e0255d9b
Kevin Thomas <kevin@admod.com>
2026-06-09 15:39:52 +0530

    Added sharded cluster

--- atlas.tf
+++ atlas.tf
@@ -1,38 +1,20 @@
-locals {
-  region_list = [for k, v in var.regions : merge(v, { key = k })]
-
-  primary_region    = local.region_list[0]
-  secondary_regions = slice(local.region_list, 1, length(local.region_list))
-}
-
 resource "mongodbatlas_advanced_cluster" "cluster" {
   project_id   = var.atlas_project_id
   name         = var.cluster_name
-  cluster_type = "REPLICASET"
+  cluster_type = "SHARDED"
 
-  replication_specs {
-    region_configs {
-      provider_name = "AZURE"
-      region_name   = local.primary_region.atlas_region
-      priority      = local.primary_region.priority
+  dynamic "replication_specs" {
+    for_each = var.regions
 
-      electable_specs {
-        instance_size = local.primary_region.instance_size
-        node_count    = local.primary_region.node_count
-      }
-    }
-
-    dynamic "region_configs" {
-      for_each = local.secondary_regions
-
-      content {
+    content {
+      region_configs {
         provider_name = "AZURE"
-        region_name   = region_configs.value.atlas_region
-        priority      = region_configs.value.priority
+        region_name   = replication_specs.value.atlas_region
+        priority      = replication_specs.value.priority
 
         electable_specs {
-          instance_size = region_configs.value.instance_size
-          node_count    = region_configs.value.node_count
+          instance_size = replication_specs.value.instance_size
+          node_count    = replication_specs.value.node_count
         }
       }
     }
