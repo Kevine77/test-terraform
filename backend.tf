55c91b0a7d8f1e0d4918f168b770ffeadf65a7d8
Kevin Thomas <kevin@admod.com>
2026-06-10 22:48:44 +0530

    Reverted

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "north-europa"
-    storage_account_name = "tfstatekevin001"
-    container_name       = "tfstate"
-    key                  = "mongodb-atlas/prod.tfstate"
-  }
-}
