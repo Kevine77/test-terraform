b22d106f4c7dcefa5f37a4166a8bc6ef5bc08819
Kevin Thomas <kevin@admod.com>
2026-06-11 18:24:13 +0530

    test removal

--- providers.tf
+++
@@ -1,9 +0,0 @@
-provider "mongodbatlas" {
-  public_key  = var.atlas_public_key
-  private_key = var.atlas_private_key
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-}
